514cae3f9a8d6238cff5cbe5be8030d9e07d3930
venkatkvikram <venkatsai.esampalli@gmail.com>
2025-04-03 13:47:50 +0530

    Changed structre && added mongodb connection string with npm mongodb package

---
+++ CreatingServerWithMongoDB/database.js
@@ -0,0 +1,8 @@
+require("mongodb")
+// Go to mongodb website
+// create a free M0 cluster
+// Create a user
+// Get the connection string
+URI = "mongodb+srv://venkatasai:apH08PfR9ekw1lAD@node-learn.0giuomi.mongodb.net/"  //database is managed on a AWS server by MongoDB
+
+//MongoDB compass allows us to acess the database
